e7acbaa2ee497d82d0f8b63d8550be2c353fb958
Oliver <oliviermedor@yahoo.com>
2015-02-26 14:33:40 -0500

    tied the review items into scope

--- public/js/controllers/searchResultsCtrl.js
+++ public/js/controllers/searchResultsCtrl.js
@@ -2,7 +2,7 @@ var app = angular.module('rateIt');
 
 app.controller('searchResultsCtrl', function($scope, dashboardService){
 	dashboardService.getAllReviews().then(function(res){
-		console.log(res);
+		$scope.allReviews = res;
 	})
 	$scope.$on('$viewAllContentLoaded', function() {
     $scope.getAllReviews();
